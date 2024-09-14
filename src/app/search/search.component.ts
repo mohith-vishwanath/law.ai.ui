47687702cc7a178ec5eb1e05fb694290bc8509db
Mohith Vishwanath <mohith.sam@hotmail.com>
2024-09-14 13:22:55 +0800

    New UI

--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -14,14 +14,52 @@ export class SearchComponent implements OnInit {
 
   public responses: SearchResponse[] = [];
 
+  public count: number = 1;
+
+  public searchBarContainer: Element | null = null;
+  public results: Element | null = null;
+
+  public showLoadingIndicator: boolean = false;
+
   ngOnInit(): void {}
+
+  ngAfterViewInit(): void {
+    this.searchBarContainer = document.querySelector('.search-bar');
+    this.results = document.querySelector('.results');
+  }
 
   openPdf(index: number): void {}
 
   search() {
+    this.showLoadingIndicator = true;
+    this.responses = [];
+    this.animate();
+    // setTimeout(() => {
+    //   this.responses.push({
+    //     title: `Title ${this.count++}`,
+    //     summary: 'Description',
+    //     citedBy: 11,
+    //     fileLink: 'URL',
+    //     relevanceScore: 0.89,
+    //   });
+    //   this.showLoadingIndicator = false;
+    // }, 900);
+
     this.service.search(this.query).subscribe((data) => {
       console.log(data);
       this.responses = data;
+      this.showLoadingIndicator = false;
     });
+  }
+
+  disableSearchButton() {
+    return this.query == '' || this.query.trim().length == 0;
+  }
+
+  animate() {
+    this.searchBarContainer?.classList.add('sticky-top');
+
+    this.results?.classList.remove('hidden');
+    this.results?.classList.add('visible');
   }
 }
