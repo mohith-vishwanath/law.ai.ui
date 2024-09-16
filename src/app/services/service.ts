b3b79db529e5bd1774103f8680d7ed501e49a1f8
Mohith Vishwanath <mohith.sam@hotmail.com>
2024-09-16 23:22:04 +0800

    in the middle

--- src/app/services/service.ts
+++ src/app/services/service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { SearchResponse } from '../models/response';
 import { HttpClient } from '@angular/common/http';
+import { environment } from 'src/environments/environment';
 
 @Injectable({ providedIn: 'root' })
 export class SearchService {
@@ -10,7 +11,7 @@ export class SearchService {
   public search(query: string): Observable<SearchResponse[]> {
     console.log(`Sending query : ${query}`);
     return this.http.get<SearchResponse[]>(
-      `http://127.0.0.1:8000/search?query=${query}`
+      `${environment.baseUrl}/search?query=${query}`
     );
   }
 }
